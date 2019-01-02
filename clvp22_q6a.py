2a3c30f951676af8409c0d1dce9a4b0c2c7eb4a3
sacktock <alex.w.goodall@gmail.com>
2019-01-02 15:08:10 +0100

    minor change in q6a and question 4e completed

--- clvp22_q6a.py
+++ clvp22_q6a.py
@@ -11,7 +11,7 @@ def msort(A): #sorts a list A in descending order
     leftA = msort(leftA) #sorting both halves of the list
     rightA = msort(rightA)
 
-    return merge(leftA,rightA) #merging to haalves together
+    return merge(leftA,rightA) #merging to halves together
 
 def merge(leftA, rightA):
     A = [] #initialise answer
